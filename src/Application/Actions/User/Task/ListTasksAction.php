0e978b5693250c7f81de764324405bcc1a0dd00c
PB <b.piotr@gmail.com>
2020-12-29 15:00:14 +0100

    Added query

--- src/Application/Actions/User/Task/ListTasksAction.php
+++ src/Application/Actions/User/Task/ListTasksAction.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 namespace App\Application\Actions\User\Task;
 
+use App\Domain\Query\GetUserTasksOfDate;
 use App\Domain\Task\Event\TasksListViewed;
-use DateTimeImmutable;
 use Psr\Http\Message\ResponseInterface as Response;
 use Ramsey\Uuid\Uuid;
 use Slim\Exception\HttpBadRequestException;
@@ -23,15 +23,15 @@ class ListTasksAction extends TaskAction
         // will throw error if user not found
         $user = $this->userRepository->findUserOfId($userId);
 
-        $date = new DateTimeImmutable(\date('Y-m-d'));
-        $tasksList = $this->taskRepository->findTaskOfUserIdAndDate($userId, $date);
+        $query = new GetUserTasksOfDate($user, $this->taskRepository);
+        $tasksList = $query->execute();
 
         $event = new TasksListViewed($tasksList, $user);
         $this->eventDispatcher->dispatch($event);
 
         $data = [
             'user' => $user,
-            'date' => $date->format('Y-m-d'),
+            'date' => date('Y-m-d'),
             'tasks' => $tasksList->getTasks()
         ];
 
---
+++ src/Domain/Query/GetUserTasksOfDate.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\Query;
+
+use App\Domain\Task\TaskRepository;
+use App\Domain\Task\ValueObject\TasksList;
+use App\Domain\User\User;
+use DateTimeImmutable;
+
+/**
+ * Class GetUserTasksOfDate
+ * @package App\Domain\Query
+ */
+class GetUserTasksOfDate implements Query
+{
+    /**
+     * @var User
+     */
+    private User $user;
+    /**
+     * @var TaskRepository
+     */
+    private TaskRepository $taskRepository;
+
+    /**
+     * GetUserTasksOfDate constructor.
+     * @param User $user
+     * @param TaskRepository $taskRepository
+     */
+    public function __construct(User $user, TaskRepository $taskRepository)
+    {
+        $this->user = $user;
+        $this->taskRepository = $taskRepository;
+    }
+
+    /**
+     * @return TasksList
+     * @throws \Exception
+     */
+    public function execute(): TasksList
+    {
+        $date = new DateTimeImmutable(\date('Y-m-d'));
+
+        return $this->taskRepository->findTaskOfUserIdAndDate($this->user->getId(), $date);
+    }
+}
